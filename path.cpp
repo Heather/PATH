2027cf172a880c59c74751163d07ce18eeae9f3a
Heather <heather@live.ru>
2015-07-06 14:07:14 +0400

    Use sorted and unique path nodes

--- path.cpp
+++ path.cpp
@@ -1,16 +1,23 @@
 #include "path.h"
 
+#include <algorithm>
+
 #include <boost/algorithm/string.hpp>
 
-const std::string Path::version = "PATH v.0.0.2";
+const std::string Path::version = "PATH v.0.0.3";
 
 Path::Path() {
   path = std::getenv("PATH");
 }
 
+/// <summary>
+/// Return sorted unique strings of path
+/// </summary>
 std::vector<std::string> Path::GetPath() {
   std::vector<std::string> strs;
   boost::split(strs, path, boost::is_any_of(";"));
+  std::sort(strs.begin(), strs.end());
+  strs.erase(std::unique(strs.begin(), strs.end()), strs.end());
   return strs;
 }
 
